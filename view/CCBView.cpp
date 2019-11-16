42534922c3ce436418d742332fda9dd758a87465
baifeng <qq394811161@gmail.com>
2019-11-16 21:04:18 +0800

    ccb view

---
+++ view/CCBView.cpp
@@ -0,0 +1,186 @@
+//
+//  CcbView.cpp
+//
+//  Created by baifeng on 2018/5/20.
+//
+
+#include "CCBView.hpp"
+
+namespace c2dext {
+
+//======================================================================================
+class CcbAnimationCompletedDelegate : public cocos2d::Ref, public cocosbuilder::CCBAnimationManagerDelegate {
+public:
+    typedef std::function<void(std::string const&)> AnimationCompletedCallback;
+    typedef std::map<std::string, AnimationCompletedCallback> ListenerMap;
+public:
+    CREATE_FUNC(CcbAnimationCompletedDelegate);
+public:
+    void setAnimationCompletedCallback(std::string const& aniName, AnimationCompletedCallback callback) {
+        mListeners[aniName] = callback;
+    }
+    void clearListeners() {
+        mListeners.clear();
+    }
+private:
+    bool init() {
+        return true;
+    }
+private:
+    virtual void completedAnimationSequenceNamed(const char *name) {
+        std::string _name(name);
+        auto iter = mListeners.find(_name);
+        if (iter != mListeners.end() && iter->second != nullptr) {
+            iter->second(_name);
+        }
+    }
+private:
+    ListenerMap mListeners;
+};
+
+//======================================================================================
+CCB::CCBPtr CCB::New() {
+    return std::make_shared<CCB>();
+}
+
+CCB::CCB():mLoader(nullptr) {
+    
+}
+
+CCB::~CCB() {
+    
+}
+
+void CCB::buildLoader() {
+    if (mLoader == nullptr) {
+        mLoader = cocosbuilder::NodeLoaderLibrary::newDefaultNodeLoaderLibrary();
+    }
+}
+
+void CCB::registerClass(std::string const& className, cocosbuilder::NodeLoader* loader) {
+    this->buildLoader();
+    mLoader->registerNodeLoader(className.c_str(), loader);
+}
+
+void CCB::removeClass(std::string const& className) {
+    this->buildLoader();
+    mLoader->unregisterNodeLoader(className.c_str());
+}
+
+cocos2d::Node* CCB::nodeFromCCB(std::string const& fileName) {
+    this->buildLoader();
+    cocosbuilder::CCBReader* reader = new cocosbuilder::CCBReader(mLoader);
+    auto node = reader->readNodeGraphFromFile(fileName.c_str());
+    reader->release();
+    return node;
+}
+
+//======================================================================================
+CCBView::CCBView():mReader(nullptr), mAniCompleted(CcbAnimationCompletedDelegate::create()) {
+    mAniCompleted->retain();
+    auto listener = cocos2d::EventListenerTouchOneByOne::create();
+    listener->onTouchBegan = CC_CALLBACK_2(CCBView::onTouchBegan, this);
+    this->registerTouchListener(listener);
+}
+
+CCBView::~CCBView() {
+    if (mReader != nullptr) {
+        // 避免引用野指针引起崩溃: rootNode == this
+        // 原因是自动释放机制延迟了析构时机导致rootNode成为了野指针
+        mReader->getAnimationManager()->setRootNode(nullptr);
+    }
+    CC_SAFE_RELEASE(this->mAniCompleted);
+    CC_SAFE_RELEASE(this->mReader);
+}
+
+void CCBView::registerTouchListener(cocos2d::EventListener* listener) {
+    auto dispatcher = this->getEventDispatcher();
+    dispatcher->removeEventListenersForTarget(this);
+    dispatcher->addEventListenerWithSceneGraphPriority(listener, this);
+}
+
+void CCBView::removeTouchListener() {
+    auto dispatcher = this->getEventDispatcher();
+    dispatcher->removeEventListenersForTarget(this);
+}
+
+void CCBView::setTouchEnabled(bool value) {
+    if (value) {
+        removeTouchListener();
+    }
+    setTouchMode(cocos2d::Touch::DispatchMode::ONE_BY_ONE);
+    Layer::setTouchEnabled(value);
+}
+
+void CCBView::setCCBReader(cocosbuilder::CCBReader* reader) {
+    CC_SAFE_RELEASE(this->mReader);
+    this->mReader = reader;
+    CC_SAFE_RETAIN(this->mReader);
+    
+    if (reader != nullptr) {
+        this->mReader->getAnimationManager()->setDelegate(mAniCompleted);
+    }
+}
+
+cocosbuilder::CCBReader* CCBView::getCCBReader() const {
+    return this->mReader;
+}
+
+void CCBView::setAnimationCompletedCallBack(std::string const& aniName, AnimationCompletedCallback callback) {
+    this->mAniCompleted->setAnimationCompletedCallback(aniName, callback);
+}
+
+void CCBView::playAnimation(std::string const& aniName, float fTweenDuration) {
+    this->mReader->getAnimationManager()->runAnimationsForSequenceNamedTweenDuration(aniName.c_str(), fTweenDuration);
+}
+
+void CCBView::removeAllAnimationCompletedCallBack() {
+    this->mAniCompleted->clearListeners();
+}
+
+bool CCBView::onTouchBegan(cocos2d::Touch *pTouch, cocos2d::Event *pEvent) {
+    cocos2d::Vec2 touchPointInView = _parent->convertToNodeSpace(pTouch->getLocation());
+    if( getBoundingBox().containsPoint(touchPointInView) ) {
+        pEvent->stopPropagation();
+        return true;
+    }
+    return false;
+}
+
+void DisableNodeTouchForTime(cocos2d::Node* node, float duration) {
+    if (node == nullptr) {
+        return;
+    }
+    using namespace cocos2d;
+    auto layer = CCBView::create();
+    layer->setContentSize(node->getContentSize());
+    layer->runAction(Sequence::create(DelayTime::create(duration), CallFuncN::create([&](Node* sender) {
+        sender->removeFromParent();
+    }), NULL));
+    node->addChild(layer);
+}
+
+#define DISABLE_TOUCH_TAG (0xE31010AE)
+
+void DisableNodeTouchForever(cocos2d::Node* node) {
+    if (node == nullptr) {
+        return;
+    }
+    using namespace cocos2d;
+    auto layer = CCBView::create();
+    layer->setContentSize(node->getContentSize());
+    layer->setTag(DISABLE_TOUCH_TAG);
+    node->addChild(layer);
+}
+
+void EnableNodeTouch(cocos2d::Node* node) {
+    if (node == nullptr) {
+        return;
+    }
+    auto view = node->getChildByTag(DISABLE_TOUCH_TAG);
+    if (view != nullptr) {
+        view->removeFromParent();
+    }
+}
+
+}
---
+++ view/CCBView.hpp
@@ -0,0 +1,159 @@
+//
+//  CcbView.hpp
+//
+//  Created by baifeng on 2018/5/20.
+//
+
+#ifndef __C2DEXT_CCB_VIEW_HPP__
+#define __C2DEXT_CCB_VIEW_HPP__
+
+#include <stdio.h>
+#include "editor-support/cocosbuilder/CocosBuilder.h"
+#include "cocos2d.h"
+
+namespace c2dext {
+
+class CCB {
+public:
+    typedef std::shared_ptr<CCB> CCBPtr;
+public:
+    static CCBPtr New();
+public:
+    CCB();
+    ~CCB();
+public:
+    void registerClass(std::string const& className, cocosbuilder::NodeLoader* loader);
+    void removeClass(std::string const& className);
+    cocos2d::Node* nodeFromCCB(std::string const& fileName);
+private:
+    void buildLoader();
+private:
+    cocosbuilder::NodeLoaderLibrary* mLoader;
+};
+
+class CcbAnimationCompletedDelegate;
+class CCBView : public cocos2d::Layer, public cocosbuilder::CCBSelectorResolver, public cocosbuilder::CCBMemberVariableAssigner, public cocosbuilder::NodeLoaderListener {
+public:
+    typedef std::function<void(std::string const&)> AnimationCompletedCallback;
+public:
+    CREATE_FUNC(CCBView);
+    CCBView();
+    ~CCBView();
+public:
+    void setAnimationCompletedCallBack(std::string const& aniName, AnimationCompletedCallback callback);
+    void playAnimation(std::string const& aniName, float fTweenDuration = 0.0f);
+    void removeAllAnimationCompletedCallBack();
+public:
+    void setCCBReader(cocosbuilder::CCBReader* reader);
+    cocosbuilder::CCBReader* getCCBReader() const;
+public:
+    void registerTouchListener(cocos2d::EventListener* listener);
+    void removeTouchListener();
+    void setTouchEnabled(bool value);
+protected:
+    virtual cocos2d::SEL_MenuHandler onResolveCCBCCMenuItemSelector(cocos2d::Ref * pTarget, const char* pSelectorName) {
+        //CCB_SELECTORRESOLVER_CCMENUITEM_GLUE(TARGET, SELECTORNAME, METHOD)
+        return nullptr;
+    }
+    virtual cocos2d::SEL_CallFuncN onResolveCCBCCCallFuncSelector(cocos2d::Ref * pTarget, const char* pSelectorName) {
+        //CCB_SELECTORRESOLVER_CALLFUNC_GLUE(TARGET, SELECTORNAME, METHOD)
+        return nullptr;
+    };
+    virtual cocos2d::extension::Control::Handler onResolveCCBCCControlSelector(cocos2d::Ref * pTarget, const char* pSelectorName) {
+        //CCB_SELECTORRESOLVER_CCCONTROL_GLUE(TARGET, SELECTORNAME, METHOD)
+        return nullptr;
+    }
+protected:
+    virtual bool onAssignCCBMemberVariable(cocos2d::Ref* pTarget, const char* pMemberVariableName, cocos2d::Node* pNode) {
+        //CCB_MEMBERVARIABLEASSIGNER_GLUE(TARGET, MEMBERVARIABLENAME, MEMBERVARIABLETYPE, MEMBERVARIABLE)
+        return false;
+    }
+    virtual bool onAssignCCBCustomProperty(cocos2d::Ref* pTarget, const char* pMemberVariableName, const cocos2d::Value& value) {
+        return false;
+    }
+protected:
+    virtual void onNodeLoaded(cocos2d::Node * pNode, cocosbuilder::NodeLoader * pNodeLoader) {
+        // nothing to do
+    }
+protected:
+    virtual bool onTouchBegan(cocos2d::Touch *pTouch, cocos2d::Event *pEvent);
+protected:
+    cocosbuilder::CCBReader* mReader;
+    CcbAnimationCompletedDelegate* mAniCompleted;
+};
+
+void DisableNodeTouchForTime(cocos2d::Node* node, float duration);
+void DisableNodeTouchForever(cocos2d::Node* node);
+void EnableNodeTouch(cocos2d::Node* node);
+
+#ifndef CCB_VIRTUAL_CREATE_NODE
+#define CCB_VIRTUAL_CREATE_NODE(T) virtual T * createNode(cocos2d::Node * pParent, cocosbuilder::CCBReader * ccbReader) override { \
+    auto node = T::create(); \
+    auto p = dynamic_cast<CCBView*>(node); \
+    if (p != nullptr) { \
+        p->setCCBReader(ccbReader); \
+    } \
+    return node; \
+}
+#endif
+
+#ifndef CCB_VIEW_LOADER_IMP
+#define CCB_VIEW_LOADER_IMP(loaderName, parentClass, nodeName) \
+class loaderName : public parentClass { \
+public: \
+    CCB_STATIC_NEW_AUTORELEASE_OBJECT_METHOD(loaderName, New); \
+protected: \
+    CCB_VIRTUAL_CREATE_NODE(nodeName); \
+}
+#endif
+
+#ifndef CCB_SPRITE_LOADER_IMP
+#define CCB_SPRITE_LOADER_IMP(loaderName, parentClass, nodeName) \
+class loaderName : public parentClass { \
+public: \
+CCB_STATIC_NEW_AUTORELEASE_OBJECT_METHOD(loaderName, New); \
+protected: \
+CCB_VIRTUAL_CREATE_NODE(nodeName); \
+protected: \
+    virtual void onHandlePropTypeSpriteFrame(cocos2d::Node * pNode, cocos2d::Node * pParent, const char* pPropertyName, cocos2d::SpriteFrame * pSpriteFrame, cocosbuilder::CCBReader * ccbReader) override { \
+        /*忽略图片加载*/ \
+    } \
+    virtual void onHandlePropTypeByte(cocos2d::Node * pNode, cocos2d::Node * pParent, const char* pPropertyName, unsigned char pByte, cocosbuilder::CCBReader * ccbReader) override { \
+        /*忽略透明值加载*/ \
+    } \
+    virtual void onHandlePropTypeColor3(cocos2d::Node * pNode, cocos2d::Node * pParent, const char * pPropertyName, cocos2d::Color3B pColor3B, cocosbuilder::CCBReader * ccbReader) override { \
+        /*忽略颜色加载*/ \
+    } \
+    virtual void onHandlePropTypeBlendFunc(cocos2d::Node * pNode, cocos2d::Node * pParent, const char * pPropertyName, cocos2d::BlendFunc pCCBBlendFunc, cocosbuilder::CCBReader * ccbReader) override { \
+        /*忽略BlendFunc加载*/ \
+    } \
+    virtual void onHandlePropTypeFlip(cocos2d::Node * pNode, cocos2d::Node * pParent, const char * pPropertyName, bool * pFlip, cocosbuilder::CCBReader * ccbReader) override { \
+        /*忽略翻转加载*/ \
+    } \
+}
+#endif
+
+#ifndef CCB_VIEW_LOADER_DEFINE
+#define CCB_VIEW_LOADER_DEFINE(loaderName, parentClass, nodeName) CCB_VIEW_LOADER_IMP(loaderName##Loader, parentClass, nodeName)
+#endif
+
+#ifndef CCB_VIEW_LOADER_FROM
+#define CCB_VIEW_LOADER_FROM(className, parentName) CCB_VIEW_LOADER_IMP(className##Loader, parentName, className)
+#endif
+
+#ifndef CCB_VIEW_LOADER
+#define CCB_VIEW_LOADER(className) CCB_VIEW_LOADER_FROM(className, cocosbuilder::NodeLoader)
+#endif
+
+/*
+ *  用于重载布局上的Sprite类型
+ */
+#ifndef CCB_SPRITE_LOADER
+#define CCB_SPRITE_LOADER(className) CCB_SPRITE_LOADER_IMP(className##Loader, cocosbuilder::NodeLoader, className)
+#endif
+
+CCB_VIEW_LOADER(CCBView);
+
+}
+
+#endif /* CcbView_hpp */
