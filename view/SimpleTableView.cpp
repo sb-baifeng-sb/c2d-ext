e6235c0462af24ff50382327342b0df19dcf4133
baifeng <qq394811161@gmail.com>
2019-11-17 17:58:42 +0800

    simple table view

---
+++ view/SimpleTableView.cpp
@@ -0,0 +1,173 @@
+//
+//  SimpleTableView.cpp
+//
+//  Created by baifeng on 2018/6/8.
+//
+
+#include "SimpleTableView.hpp"
+
+namespace c2dext {
+
+SimpleTableView* SimpleTableView::create(cocos2d::Size const& size) {
+    auto view = new (std::nothrow) SimpleTableView;
+    if (view && view->initWithViewSize(size)) {
+        view->autorelease();
+        return view;
+    }
+    return nullptr;
+}
+
+SimpleTableView::SimpleTableView() {
+    this->setDataSource(this);
+    this->setDelegate(this);
+}
+
+SimpleTableView::~SimpleTableView() {
+}
+
+void SimpleTableView::setContentSize(cocos2d::Size const& size) {
+    TableView::setContentSize(size);
+    this->initWithViewSize(size);
+}
+
+void SimpleTableView::scrollViewDidScroll(ScrollView* view) {
+
+}
+
+void SimpleTableView::scrollViewDidZoom(ScrollView* view) {
+
+}
+
+void SimpleTableView::tableCellTouched(TableView* table, TableViewCell* cell) {
+
+}
+
+void SimpleTableView::tableCellHighlight(TableView* table, TableViewCell* cell) {
+
+}
+
+void SimpleTableView::tableCellUnhighlight(TableView* table, TableViewCell* cell) {
+
+}
+
+void SimpleTableView::tableCellWillRecycle(TableView* table, TableViewCell* cell) {
+
+}
+
+cocos2d::Size SimpleTableView::cellSizeForTable(TableView* table) {
+    return cocos2d::Size::ZERO;
+}
+
+cocos2d::Size SimpleTableView::tableCellSizeForIndex(TableView* table, ssize_t idx) {
+    return cellSizeForTable(table);
+}
+
+TableViewCell* SimpleTableView::tableCellAtIndex(TableView *table, ssize_t idx) {
+    return nullptr;
+}
+
+ssize_t SimpleTableView::numberOfCellsInTableView(TableView *table) {
+    return 0;
+}
+
+//=======================================================================
+
+void QuickTableView::scrollViewDidScroll(ScrollView* view) {
+    auto iter = mCallback.find("scrollViewDidScroll");
+    if (iter != mCallback.end()) {
+        Context c(this, nullptr, 0);
+        Result r();
+        iter->second(c, r);
+    }
+}
+
+void QuickTableView::scrollViewDidZoom(ScrollView* view) {
+    auto iter = mCallback.find("scrollViewDidZoom");
+    if (iter != mCallback.end()) {
+        Context c(this, nullptr, 0);
+        Result r();
+        iter->second(c, r);
+    }
+}
+
+void QuickTableView::tableCellTouched(TableView* table, TableViewCell* cell) {
+    auto iter = mCallback.find("tableCellTouched");
+    if (iter != mCallback.end()) {
+        Context c(table, cell, 0);
+        Result r();
+        iter->second(c, r);
+    }
+}
+
+void QuickTableView::tableCellHighlight(TableView* table, TableViewCell* cell) {
+    auto iter = mCallback.find("tableCellHighlight");
+    if (iter != mCallback.end()) {
+        Context c(table, cell, 0);
+        Result r();
+        iter->second(c, r);
+    }
+}
+
+void QuickTableView::tableCellUnhighlight(TableView* table, TableViewCell* cell) {
+    auto iter = mCallback.find("tableCellUnhighlight");
+    if (iter != mCallback.end()) {
+        Context c(table, cell, 0);
+        Result r();
+        iter->second(c, r);
+    }
+}
+
+void QuickTableView::tableCellWillRecycle(TableView* table, TableViewCell* cell) {
+    auto iter = mCallback.find("tableCellWillRecycle");
+    if (iter != mCallback.end()) {
+        Context c(table, cell, 0);
+        Result r();
+        iter->second(c, r);
+    }
+}
+
+cocos2d::Size QuickTableView::cellSizeForTable(TableView* table) {
+    auto iter = mCallback.find("cellSizeForTable");
+    if (iter != mCallback.end()) {
+        Context c(table, nullptr, 0);
+        Result r();
+        iter->second(c, r);
+        return r.cellSize;
+    }
+    return cocos2d::Size::ZERO;
+}
+
+cocos2d::Size QuickTableView::tableCellSizeForIndex(TableView* table, ssize_t idx) {
+    auto iter = mCallback.find("tableCellSizeForIndex");
+    if (iter != mCallback.end()) {
+        Context c(table, nullptr, idx);
+        Result r();
+        iter->second(c, r);
+        return r.cellSize;
+    }
+    return cellSizeForTable(table);
+}
+
+TableViewCell* QuickTableView::tableCellAtIndex(TableView *table, ssize_t idx) {
+    auto iter = mCallback.find("tableCellAtIndex");
+    if (iter != mCallback.end()) {
+        Context c(table, nullptr, idx);
+        Result r();
+        iter->second(c, r);
+        return r.cell;
+    }
+    return nullptr;
+}
+
+ssize_t QuickTableView::numberOfCellsInTableView(TableView *table) {
+    auto iter = mCallback.find("tableCellAtIndex");
+    if (iter != mCallback.end()) {
+        Context c(table, nullptr, 0);
+        Result r();
+        iter->second(c, r);
+        return r.size;
+    }
+    return 0;
+}
+
+}
---
+++ view/SimpleTableView.hpp
@@ -0,0 +1,87 @@
+//
+//  SimpleTableView.hpp
+//
+//  Created by baifeng on 2018/6/8.
+//
+
+#ifndef __C2DEXT_SIMPLE_TABLEVIEW_H__
+#define __C2DEXT_SIMPLE_TABLEVIEW_H__
+
+#include <stdio.h>
+#include "TableView-ext.hpp"
+
+namespace c2dext {
+
+class SimpleTableView : public TableView, public TableViewDataSource, public TableViewDelegate {
+public:
+    static SimpleTableView* create(cocos2d::Size const& size);
+    CREATE_FUNC(SimpleTableView);
+    SimpleTableView();
+    ~SimpleTableView();
+public:
+    virtual void setContentSize(cocos2d::Size const& size) override;
+protected:
+    // scroll view delegate
+    virtual void scrollViewDidScroll(ScrollView* view) override;
+    virtual void scrollViewDidZoom(ScrollView* view) override;
+protected:
+    // table view delegate
+    virtual void tableCellTouched(TableView* table, TableViewCell* cell) override;
+    virtual void tableCellHighlight(TableView* table, TableViewCell* cell) override;
+    virtual void tableCellUnhighlight(TableView* table, TableViewCell* cell) override;
+    virtual void tableCellWillRecycle(TableView* table, TableViewCell* cell) override;
+protected:
+    // dataSource
+    virtual cocos2d::Size cellSizeForTable(TableView* table) override; // no use
+    virtual cocos2d::Size tableCellSizeForIndex(TableView* table, ssize_t idx) override;
+    virtual TableViewCell* tableCellAtIndex(TableView *table, ssize_t idx) override;
+    virtual ssize_t numberOfCellsInTableView(TableView *table) override;
+};
+
+class QuickTableView : public SimpleTableView {
+public:
+    class Context {
+    public:
+        TableView* sender;
+        TableViewCell* cell;
+        ssize_t index;
+    public:
+        Context():sender(nullptr), cell(nullptr), index(0){}
+        Context(TableView* table, TableViewCell* cell, ssize_t idx):sender(table), cell(cell), index(idx){}
+    };
+    class Result {
+    public:
+        cocos2d::Size cellSize;
+        TableViewCell* cell;
+        ssize_t size;
+    public:
+        Result():cell(nullptr), size(0){}
+        Result(cocos2d::Size const& cellSize, TableViewCell* cell, ssize_t size):cellSize(cellSize), cell(cell), size(size){}
+    };
+    typedef std::function<void(Context& c, Result& r)> CallBack;
+    typedef std::map<std::string, CallBack> CallBackMap;
+public:
+    void setCallback(std::string const& apiName, CallBack const& f);
+protected:
+    // scroll view delegate
+    virtual void scrollViewDidScroll(ScrollView* view) override;
+    virtual void scrollViewDidZoom(ScrollView* view) override;
+protected:
+    // table view delegate
+    virtual void tableCellTouched(TableView* table, TableViewCell* cell) override;
+    virtual void tableCellHighlight(TableView* table, TableViewCell* cell) override;
+    virtual void tableCellUnhighlight(TableView* table, TableViewCell* cell) override;
+    virtual void tableCellWillRecycle(TableView* table, TableViewCell* cell) override;
+protected:
+    // dataSource
+    virtual cocos2d::Size cellSizeForTable(TableView* table) override; // no use
+    virtual cocos2d::Size tableCellSizeForIndex(TableView* table, ssize_t idx) override;
+    virtual TableViewCell* tableCellAtIndex(TableView *table, ssize_t idx) override;
+    virtual ssize_t numberOfCellsInTableView(TableView *table) override;
+private:
+    CallBackMap mCallback;
+};
+
+}
+
+#endif /* SimpleTableView_hpp */
