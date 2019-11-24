2bf359f71687b675b8522b524868a965b8f45ca9
baifeng <qq394811161@gmail.com>
2019-11-24 14:39:32 +0800

    click view double click view

--- view/TouchableView.cpp
+++ view/TouchableView.cpp
@@ -57,4 +57,52 @@ void TouchableView::onTouchCancelled(cocos2d::Touch *touch, cocos2d::Event *unus
     this->get("onTouchCancelled")(c);
 }
 
+ClickableView::ClickableView() {
+    this->setTouchMode(cocos2d::Touch::DispatchMode::ONE_BY_ONE);
+    this->setTouchEnabled(true);
+}
+
+void ClickableView::setClick(ClickCall const& f) {
+    this->mClicked = f;
+}
+
+bool ClickableView::onTouchBegan(cocos2d::Touch *touch, cocos2d::Event *unused_event) {
+    cocos2d::Vec2 touchPointInView = _parent->convertToNodeSpace(touch->getLocation());
+    if( getBoundingBox().containsPoint(touchPointInView) ) {
+        if (this->mClicked != nullptr) {
+            this->mClicked(this);
+        }
+    }
+    return false;
+}
+
+DoubleClickView::DoubleClickView():mClicked(false) {
+    this->setTouchMode(cocos2d::Touch::DispatchMode::ONE_BY_ONE);
+    this->setTouchEnabled(true);
+}
+
+void DoubleClickView::setClick(ClickCall const& f) {
+    this->mClickCall = f;
+}
+
+void DoubleClickView::onCancel(float dt) {
+    this->mClicked = false;
+}
+
+bool DoubleClickView::onTouchBegan(cocos2d::Touch *touch, cocos2d::Event *unused_event) {
+    cocos2d::Vec2 touchPointInView = _parent->convertToNodeSpace(touch->getLocation());
+    if( getBoundingBox().containsPoint(touchPointInView) ) {
+        if (this->mClicked) {
+            this->mClicked = false;
+            if (this->mClickCall != nullptr) {
+                this->mClickCall(this);
+            }
+        } else {
+            this->mClicked = true;
+            this->scheduleOnce(CC_SCHEDULE_SELECTOR(DoubleClickView::onCancel), 0.25f);
+        }
+    }
+    return false;
+}
+
 }
--- view/TouchableView.hpp
+++ view/TouchableView.hpp
@@ -36,6 +36,36 @@ private:
     TouchFuncMap mCallback;
 };
 
+class ClickableView : public cocos2d::Layer {
+public:
+    typedef std::function<void(cocos2d::Node* sender)> ClickCall;
+public:
+    CREATE_FUNC(ClickableView);
+    ClickableView();
+public:
+    void setClick(ClickCall const& f);
+private:
+    bool onTouchBegan(cocos2d::Touch *touch, cocos2d::Event *unused_event) override;
+private:
+    ClickCall mClicked;
+};
+
+class DoubleClickView : public cocos2d::Layer {
+public:
+    typedef std::function<void(cocos2d::Node* sender)> ClickCall;
+public:
+    CREATE_FUNC(DoubleClickView);
+    DoubleClickView();
+public:
+    void setClick(ClickCall const& f);
+private:
+    bool onTouchBegan(cocos2d::Touch *touch, cocos2d::Event *unused_event) override;
+    void onCancel(float dt);
+private:
+    bool mClicked;
+    ClickCall mClickCall;
+};
+
 }
 
 #endif
